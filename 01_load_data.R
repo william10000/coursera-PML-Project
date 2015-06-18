a1e7687c1b37bd42346b3cff5e95ae45480770de
William Wan <william10000@gmail.com>
2015-06-18 05:06:47 -0600

    1st commit with load data script

---
+++ 01_load_data.R
@@ -0,0 +1,11 @@
+# Practical Machine Learning project
+
+# William Wan 
+# 2015-06-17
+
+# this script loads files and creates traning sets
+
+
+train <- read.csv("pml-training.csv")
+test <- read.csv("pml-testing.csv")
+
